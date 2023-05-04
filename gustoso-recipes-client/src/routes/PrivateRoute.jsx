f37fd0d0339910d60559dbf05917de976c88317d
Ahmed Omar Salim Adnan <44505932+RionelxSlovesky@users.noreply.github.com>
2023-05-04 22:38:08 +0600

    Implemented Spinner
    Now showing spinner while loading data

--- gustoso-recipes-client/src/routes/PrivateRoute.jsx
+++ gustoso-recipes-client/src/routes/PrivateRoute.jsx
@@ -1,13 +1,14 @@
 import React, { useContext } from 'react';
 import { Navigate } from "react-router-dom";
 import { AuthContext } from '../providers/AuthProvider';
+import { RingLoader } from 'react-spinners'
 
 const PrivateRoute = ({children}) => {
 
     const {user,loading} = useContext(AuthContext)
 
     if(loading) {
-        return <h1>Loading</h1>
+        return <RingLoader className='mx-auto' color="#2B3856" />
     }else {
         return (user ? children : <Navigate to='/auth/login'></Navigate>)
     }
