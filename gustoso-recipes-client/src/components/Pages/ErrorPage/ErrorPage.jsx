336e63b397bab805f188a87c8512d5a99948524a
Ahmed Omar Salim Adnan <44505932+RionelxSlovesky@users.noreply.github.com>
2023-05-04 15:56:58 +0600

    Created Error Page
    Now showing error page for trying to visit parts of the website which does not exist

---
+++ gustoso-recipes-client/src/components/Pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './ErrorPage.css'
+
+const ErrorPage = () => {
+    return (
+        <img src={'https://i.ibb.co/vwJrWmq/error404.png'} alt="" className='error'/>
+    );
+};
+
+export default ErrorPage;
